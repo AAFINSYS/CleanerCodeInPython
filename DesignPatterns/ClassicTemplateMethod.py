aefffd412a62b070aaeca5a408c9453668e42256
AAFINSYS <43729299+aafinsys@users.noreply.github.com>
2019-01-20 18:48:07 +0100

    template method design pattern
    Signed-off-by: Davy Bureau <davybureau@yahoo.fr>

---
+++ DesignPatterns/ClassicTemplateMethod.py
@@ -0,0 +1,60 @@
+import unittest
+import sys
+from abc import ABC, abstractmethod
+
+
+class Algorithm(ABC):
+
+    def template_method(self):
+        """Generic pattern of operations to perform. DO NOT override.
+        """
+        result1 = self.specific_processing_1()
+        result2 = self.specific_processing_2()
+        result3 = self.common_processings()
+
+        return result3 + result1 + result2
+
+    @abstractmethod
+    def specific_processing_1(self):
+        """You HAVE TO override this method n a subclass."""
+        pass
+
+    @abstractmethod
+    def specific_processing_2(self):
+        """You HAVE TO override this method n a subclass."""
+        pass
+
+    def common_processings(self):
+        """You CAN override this method. Here it ccntains common operations to perform in any case"""
+        return "TemplateMethod"
+
+
+class Sibling1(Algorithm):
+    def specific_processing_1(self):
+        return "_SiblingClass1Method1"
+
+    def specific_processing_2(self):
+        return "_siblingClass1Method2"
+
+class Sibling2(Algorithm):
+    def specific_processing_1(self):
+        return "_SiblingClass2Method1"
+
+    def specific_processing_2(self):
+        return "_siblingClass2Method2"
+
+class Test_Temp1lateMethodDesignPattern(unittest.TestCase):
+    def test_shouldReturnSiblingClassResult_When_CallingTemplateMethodOnSiblingClass(self):
+        sib1 = Sibling1()
+        actual = sib1.template_method()
+
+        self.assertEqual(actual, "TemplateMethod_SiblingClass1Method1_siblingClass1Method2")
+
+    def test_shouldReturnSiblingClassResult_When_CallingTemplateMethodOnSiblingClass(self):
+        sib1 = Sibling2()
+        actual = sib1.template_method()
+
+        self.assertEqual(actual, "TemplateMethod_SiblingClass2Method1_siblingClass2Method2")
+
+if __name__ == "__main__":
+    unittest.main()

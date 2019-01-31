279fd05c7eb005fe385ab9671ff934aa159936ed
AAFINSYS <43729299+aafinsys@users.noreply.github.com>
2019-01-31 16:27:58 +0100

    duplicated code - roman numeral
    Signed-off-by: Davy Bureau <davybureau@yahoo.fr>

--- CodeSmells/DeadCodeSmell.py
+++ CodeSmells/DeadCodeSmell.py
@@ -1,4 +1,28 @@
 import unittest
 
+
+def my_function(activated):
+    return_value = "NOT FOUND"
+
+    if activated:
+        return_value = "Live code"
+
+        # I added some comment here just to say I commented the code below in case
+        # related_value_i_dont_need_anymore
+        # if len(return_value) == 0:
+        #   return_value = "Default value"
+
+        if not activated:
+            check_version = return_value.lower()
+
+    return return_value
+
+
+class TestDeadCode(unittest.TestCase):
+    def test_should_return_the_correct_value_when_function_is_called(self):
+        actual = my_function(True)
+
+        self.assertEqual(actual, "Live code")
+
 if __name__ == "__main__":
     unittest.main()

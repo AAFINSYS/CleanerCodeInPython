092eb02c82f66c0f79ca6bf09f4962d000477105
AAFINSYS <43729299+aafinsys@users.noreply.github.com>
2019-01-31 15:14:59 +0100

    misc
    Signed-off-by: Davy Bureau <davybureau@yahoo.fr>

--- Katas/BowlingKata.py
+++ Katas/BowlingKata.py
@@ -28,7 +28,7 @@ class Game(object):
     def roll(self, pins):
         pass
 
-    def total_score(self):
+    def score(self):
         return 0
 
 
@@ -39,19 +39,19 @@ class GameTest(unittest.TestCase):
 
     def test_gutter_game(self):
         self._roll_many(0, 20)
-        self.assertEqual(0, self.game.total_score())
+        self.assertEqual(0, self.game.score())
 
     @unittest.skip
     def test_all_ones(self):
         self._roll_many(1, 20)
-        self.assertEqual(20, self.game.total_score())
+        self.assertEqual(20, self.game.score())
 
     @unittest.skip
     def test_one_spare(self):
         self._roll_spare()
         self.game.roll(3)
         self._roll_many(0, 17)
-        self.assertEqual(16, self.game.total_score())
+        self.assertEqual(16, self.game.score())
 
     @unittest.skip
     def test_one_strike(self):
@@ -59,19 +59,19 @@ class GameTest(unittest.TestCase):
         self.game.roll(3)
         self.game.roll(4)
         self._roll_many(0, 16)
-        self.assertEqual(24, self.game.total_score())
+        self.assertEqual(24, self.game.score())
 
     @unittest.skip
     def test_perfect_game(self):
         self._roll_many(10, 12)
-        self.assertEqual(300, self.game.total_score())
+        self.assertEqual(300, self.game.score())
 
     @unittest.skip
     def test_simple_game(self):
         for pins in [1, 4, 4, 5, 6, 4, 5, 5,
                      10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]:
             self.game.roll(pins)
-        self.assertEqual(133, self.game.total_score())
+        self.assertEqual(133, self.game.score())
 
     def _roll_many(self, pins, num):
         for i in range(num):

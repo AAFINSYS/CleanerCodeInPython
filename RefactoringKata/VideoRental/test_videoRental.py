794d86576aebeae106428773a8d5957d6e598830
AAFINSYS <43729299+aafinsys@users.noreply.github.com>
2019-02-02 19:26:43 +0100

    Video rental kata + first test
    Signed-off-by: Davy Bureau <davybureau@yahoo.fr>

--- RefactoringKata/VideoRental/test_videoRental.py
+++ RefactoringKata/VideoRental/test_videoRental.py
@@ -1,4 +1,18 @@
 import unittest
+from RefactoringKata.VideoRental.VideoRental import Customer, Rental, Movie
+
+
+class Test_VideoRental(unittest.TestCase):
+    def test_should_when(self):
+        customer = Customer("John")
+        movie = Movie("Fantasia", Movie.Children)
+        rental = Rental(movie, 1)
+
+        customer.add_rental(rental)
+
+        actual = customer.statement()
+
+        self.assertEqual(actual, "Rental Record for John\n\tFantasia\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points")
 
 if __name__ == "__main__":
     unittest.main()

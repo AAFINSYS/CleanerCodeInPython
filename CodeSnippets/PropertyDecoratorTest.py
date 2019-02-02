d37e0209e23e060a7146092480c0f935dc1c48c1
AAFINSYS <43729299+aafinsys@users.noreply.github.com>
2019-02-02 12:49:21 +0100

    property
    Signed-off-by: Davy Bureau <davybureau@yahoo.fr>

--- CodeSnippets/PropertyDecoratorTest.py
+++ CodeSnippets/PropertyDecoratorTest.py
@@ -27,8 +27,21 @@ class PropertyRectangle:
 
         self._width = new_value
 
+    @length.setter
+    def length(self, new_value):
+        if new_value <= 0:
+            raise ValueError
+
+        self._length = new_value
+
+
 def set_property_rectangle_width_to_negative_number(property_rectangle):
     property_rectangle.width = -10
+
+
+def set_property_rectangle_length_to_negative_number(property_rectangle):
+    property_rectangle.length = -40
+
 
 class TestPropertyDecorator(unittest.TestCase):
     def test_should_return_same_length_and_width_for_two_different_classes_when_using_direct_access_or_property_decorator(self):
@@ -42,6 +55,8 @@ class TestPropertyDecorator(unittest.TestCase):
         property_rectangle = PropertyRectangle(10, 20)
 
         self.assertRaises(ValueError, set_property_rectangle_width_to_negative_number, property_rectangle)
+        self.assertRaises(ValueError, set_property_rectangle_length_to_negative_number, property_rectangle)
+
 
 if __name__ == "__main__":
     unittest.main()
